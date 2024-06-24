267ed4975fb5f9e1ad5b5f587df7f63773162508
juliatduffy <jtduffy7@gmail.com>
2024-06-24 14:27:48 -0600

    Home page/ formatting edits.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// Menu bar stuff
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.hamburger-icon');
     const navbar = document.querySelector('.navbar');
@@ -7,3 +9,33 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
   
+// Carousel stuff
+const images = document.getElementsByClassName('carousel-image');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+currImage = 0;
+images[currImage].classList.add('active');
+
+
+function showImage(index) {
+  images[currImage].classList.remove('active');
+  currImage++;
+  
+  if(currImage >= images.length) {
+    currImage = 0;
+  }
+  else if(currImage < 0) {
+    currImage = images.length - 1;
+  }
+  
+  images[currImage].classList.add('active');
+}
+
+prevButton.addEventListener('click', () => {
+  showImage(currImage - 1);
+});
+
+nextButton.addEventListener('click', () => {
+  showImage(currImage + 1);
+});
+
